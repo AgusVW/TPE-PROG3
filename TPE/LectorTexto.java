16a1df4b6df1456639c20415e43dd0cdddcf5e48
Agustin Van Waarde <agusvanwaarde2006@gmail.com>
2025-05-07 21:05:40 -0300

    primer committ tpe

---
+++ TPE/LectorTexto.java
@@ -0,0 +1,30 @@
+package TPE;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import tp1.mainn;
+
+public class LectorTexto {
+    public static void main(String [] args){       
+        try {
+            ArrayList<Maquina> maquinas=new ArrayList<>();
+            List<String> lines = Files.readAllLines(Paths.get("TPE/text.txt"));
+            int piezasAproducir=Integer.parseInt(lines.get(0));
+            for(int i=1;i < lines.size();i++){
+                String[] cadenas=lines.get(i).split(",");
+                int numPiezas=Integer.parseInt(cadenas[1]);
+                maquinas.add(new Maquina(cadenas[0],numPiezas));
+                //System.out.println(lines.get(i));
+            }
+            System.out.println(piezasAproducir);
+            System.out.println(maquinas);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println(System.getProperty("user.dir")); sirve para encontrar el directorio donde esta parado
+    }
+}
---
+++ TPE/Maquina.java
@@ -0,0 +1,36 @@
+package TPE;
+
+public class Maquina {
+    private String nombre;
+    private int cantPiezas;
+
+    public Maquina(String nombre,int cantPiezas){
+        this.nombre=nombre;
+        this.cantPiezas=cantPiezas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantPiezas() {
+        return cantPiezas;
+    }
+
+    public void setCantPiezas(int cantPiezas) {
+        this.cantPiezas = cantPiezas;
+    }
+
+    @Override
+    public String toString() {
+        return "Maquina [nombre=" + nombre + ", cantPiezas=" + cantPiezas + "]";
+    }
+
+    
+
+    
+}
